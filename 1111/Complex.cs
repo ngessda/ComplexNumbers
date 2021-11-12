cdf154829b37b2c5ac028cd6d05a98e895cc046d
Igor Kind <86460417+ngessda@users.noreply.github.com>
2021-11-12 23:58:12 +0900

    none

---
+++ 1111/Complex.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1111
+{
+    public class Complex
+    {
+        private double re;
+        private double im;
+
+        public double Re
+        {
+            get
+            {
+                return re;
+            }
+            set
+            {
+                re = value;
+            }
+        }
+        public double Im
+        {
+            get
+            {
+                return im;
+            }
+            set
+            {
+                im = value;
+            }
+        }
+        public static Complex operator+(Complex a,Complex b)
+        {
+            Complex c = new Complex();
+            c.re = a.re + b.re;
+            c.im = a.im + b.im;
+            return c;
+        }
+        public static Complex operator-(Complex a, Complex b)
+        {
+            Complex c = new Complex();
+            c.re = a.re - b.re;
+            c.im = a.im - b.im;
+            return c;
+        }
+        public static Complex operator*(Complex a, Complex b)
+        {
+            Complex c = new Complex();
+            c.re = a.re * b.re;
+            c.im = a.im * b.im;
+            return c;
+        }
+        public static Complex operator/(Complex a, Complex b)
+        {
+            try
+            {
+                if(a.re == 0 && b.re ==0)
+                {
+                    throw new Exception();
+                }
+            }
+            catch
+            {
+                throw new Exception(")))))");
+            }
+            Complex c = new Complex();
+            c.re = a.re / b.re;
+            c.re = a.im / b.im;
+            return c;
+        }
+    }
+}
---
+++ 1111/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-oe действ. число:");
+            double re1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2-oe действ. число:");
+            double re2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("1-oe мнимое число:");
+            double im1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2-oe мнимое число:");
+            double im2 = Convert.ToDouble(Console.ReadLine());
+            Complex re = new Complex();
+            Complex im = new Complex();
+            re.Re = re1;
+            re.Im = im1;
+            im.Re = re2;
+            im.Im = im2;
+            Console.WriteLine("{0} + {1}i", (re / im).Re, (re / im).Im);
+            Console.ReadKey();
+        }
+    }
+}
